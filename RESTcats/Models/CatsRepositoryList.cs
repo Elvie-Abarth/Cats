6be024fbe109d06e2b5c460a3bc5fd84cd033ace
move <move@zealand.dk>
2026-03-20 11:16:20 +0100

    prøve at tilføje fejl

--- RESTcats/Models/CatsRepositoryList.cs
+++ RESTcats/Models/CatsRepositoryList.cs
@@ -5,7 +5,7 @@ namespace RESTcats.Models
     public class CatsRepositoryList : ICatsRepository
     {
         private readonly List<Cat> cats = new();
-        private int nextId = 1;
+        private int nextId = 2;
 
         public CatsRepositoryList(bool includeData = false)
         {
